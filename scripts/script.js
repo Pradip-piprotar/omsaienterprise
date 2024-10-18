c3a8f8d23543529737bd90dc9f08d00cc5d1881f
Pradip <pradippiprotar05@gmail.com>
2024-10-18 16:20:10 +0530

    new

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Form Submitted!');
+    // Here you can add AJAX or other logic to send the form data to a server
+});
